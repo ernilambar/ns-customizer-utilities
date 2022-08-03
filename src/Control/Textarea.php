fdb55e506567e2bc148ea2a501032d319f82f86f
Nilambar Sharma <nilambar@outlook.com>
2022-08-03 21:30:09 +0545

    feat: textarea field

---
+++ src/Control/Textarea.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Textarea control
+ *
+ * @package NSCU
+ */
+
+namespace Nilambar\CustomizerUtils\Control;
+
+use WP_Customize_Control;
+
+/**
+ * Textarea control class.
+ *
+ * @since 1.0.0
+ *
+ * @see WP_Customize_Control
+ */
+class Textarea extends WP_Customize_Control {
+
+	/**
+	 * Control type.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	public $type = 'nscu-textarea';
+
+	/**
+	 * Export data to JS.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array JSON data.
+	 */
+	public function json() {
+		$data = parent::json();
+
+		$data['id']           = $this->type . '-' . $this->id;
+		$data['value']        = $this->value();
+		$data['link']         = $this->get_link();
+		$data['defaultValue'] = $this->setting->default;
+
+		return $data;
+	}
+
+	/**
+	 * Enqueue scripts and styles.
+	 *
+	 * @since 1.0.0
+	 */
+	public function enqueue() {
+		wp_enqueue_style( 'nscu-controls' );
+		wp_enqueue_script( 'nscu-controls' );
+	}
+
+	/**
+	 * Render JS template.
+	 *
+	 * @since 1.0.0
+	 */
+	public function content_template() {
+		?>
+		<# if ( data.label ) { #>
+			<label class="customize-control-title" for="{{ data.id }}">{{ data.label }}</label>
+		<# } #>
+		<# if ( data.description ) { #>
+			<span class="description customize-control-description">{{ data.description }}</span>
+		<# } #>
+
+		<textarea name="_customize-textarea-{{ data.id }}" id="{{ data.id }}" {{{ data.link }}}>{{{ data.value }}}</textarea>
+		<?php
+	}
+
+	/**
+	 * Render content.
+	 *
+	 * @since 1.0.0
+	 */
+	public function render_content() {}
+}
