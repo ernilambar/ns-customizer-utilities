9360da1f89d1d4a32769d09408305dcbcc08d6e0
Nilambar Sharma <nilambar@outlook.com>
2022-08-09 11:22:08 +0545

    fix: version bump

--- init.php
+++ init.php
@@ -7,7 +7,7 @@
 
 namespace Nilambar\CustomizerUtils;
 
-define( 'NSCU_VERSION' , '1.0.3' );
+define( 'NSCU_VERSION' , '1.0.6' );
 
 if ( ! defined( 'NSCU_DIR' ) ) {
 	define( 'NSCU_DIR' , rtrim( plugin_dir_path( __FILE__ ), '/' ) );
